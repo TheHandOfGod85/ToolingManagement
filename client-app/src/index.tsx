849a8d02a0472d0acbf3f8c2de44f674334a8d79
Daniele Del Piano <daniele.delpiano30@gmail.com>
2022-12-10 10:57:32 +0000

    starting from scratch with react

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,17 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./app/layout/style.css";
-import App from "./app/layout/App";
-import { BrowserRouter } from "react-router-dom";
-import { store, StoreContext } from "./app/stores/store";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter>
-    <StoreContext.Provider value={store}>
-      <App />
-    </StoreContext.Provider>
-  </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
